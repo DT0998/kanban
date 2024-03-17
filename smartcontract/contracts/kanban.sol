ceee1d89d59108f1854c4a18b9c9515d6cc1a39d
DT0998 <duongkytrieuvn@gmail.com>
2024-03-17 23:19:44 +0700

    feat: update api and logic smart contract

--- smartcontract/contracts/kanban.sol
+++ smartcontract/contracts/kanban.sol
@@ -2,63 +2,31 @@
 pragma solidity ^0.8.9;
 
 contract Kanban {
-    //Define the Owner of the smart contract
-
-    address payable owner;
-
-    constructor() {
-        owner = payable(msg.sender);
-    }
-
     //Create Struct and Mappping for request, transaction & name
 
-    struct request {
-        address requestor;
+    struct user {
         uint256 amount;
+        address userAddressWallet;
     }
 
-    struct sendReceive {
-        string action;
-        uint256 amount;
-        address otherPartyAddress;
-    }
-
-    mapping(address => request[]) requests;
-    mapping(address => sendReceive[]) history;
-
-    //Create a user subscribe request premium
-
-    function createSubscribeRequestPremium() public {
-        // tìm cách yêu cầu ngược lại
-        request memory newRequest;
-        // pay for owner web app
-        newRequest.requestor = owner;
-        newRequest.amount = 1000000000000000000 wei;
-        requests[msg.sender].push(newRequest);
-    }
+    // mapping(address => request[]) requests;
+    mapping(address => user[]) history;
+    mapping(address => user[]) users;
 
     //user subscribe premium
-
-    function subscribeRequestPremium() public payable {
-        request[] storage myRequests = requests[owner];
-        request storage payableRequest = myRequests[0];
-        // owner cannot pay themselves
-        require(msg.sender != owner, "Contract owner cannot pay themselves");
-        // request only be payable to the contract owner
-        require(
-            payableRequest.requestor == owner,
-            "Request is only payable to the contract owner"
-        );
+    function subscribeRequestPremium(
+        address sender,
+        address receiver
+    ) public payable {
         // pay with one matic
-        uint256 toPay = 1000000000000000000 wei;
-        require(msg.value == (toPay), "Pay Correct Amount");
+        uint256 toPay = msg.value;
 
-        payable(owner).transfer(toPay);
+        // check if the sender is not the owner
+        require(sender != receiver, "Contract owner cannot pay themselves");
 
-        addHistorySubscribePremium(msg.sender, payableRequest.amount);
+        payable(receiver).transfer(toPay);
 
-        myRequests[0] = myRequests[myRequests.length - 1];
-        myRequests.pop();
+        addHistorySubscribePremium(sender, toPay);
     }
 
     // history user subscribe premium
@@ -67,35 +35,17 @@ contract Kanban {
         uint256 _amount
     ) private {
         // history sender
-        sendReceive memory newSend;
-        newSend.action = "Send";
+        user memory newSend;
         newSend.amount = _amount;
+        newSend.userAddressWallet = sender;
         history[sender].push(newSend);
-    }
-
-    //Get user requests sent to a subscribe premium
-
-    function getMyRequestsSubscribePremium(
-        address _user
-    ) public view returns (address[] memory, uint256[] memory) {
-        address[] memory addrs = new address[](requests[_user].length);
-        uint256[] memory amnt = new uint256[](requests[_user].length);
-
-        for (uint i = 0; i < requests[_user].length; i++) {
-            request storage myRequests = requests[_user][i];
-            addrs[i] = myRequests.requestor;
-            amnt[i] = myRequests.amount;
-        }
-
-        return (addrs, amnt);
     }
 
     //Get user historic transactions subscribe premium
 
     function getMyHistorySubscribePremium(
-        address _user
-    ) public view returns (sendReceive[] memory) {
-        return history[_user];
+        address _sender
+    ) public view returns (user[] memory) {
+        return history[_sender];
     }
-
 }
