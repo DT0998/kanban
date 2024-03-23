25dc09c0e2242e4b095117b5c842580d6522753c
Duong Trieu <duongkytrieuvn@gmail.com>
2024-03-23 16:21:18 +0700

    Delete backend/API directory

--- backend/API/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-const userRoutes = require("./user.routes");
-const authRoutes = require("./auth.routes");
-const premiumRoutes = require("./premium.routes");
-
-module.exports = {
-  userRoutes,
-  authRoutes,
-  premiumRoutes,
-};
