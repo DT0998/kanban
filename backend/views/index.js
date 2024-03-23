89d403f564b99ad3f1285db513ff911b5e0a5217
DT0998 <duongkytrieuvn@gmail.com>
2024-03-23 21:51:32 +0700

    fix: change correct path

--- backend/views/index.js
+++ backend/views/index.js
@@ -5,13 +5,13 @@ import ejs from "ejs";
 
 const renderViews = (app) => {
   // render pages
-  app.set("view engine", "ejs");
+  app.set('view engine', 'ejs');
   // This was we can keep everything inside our src folder!!
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  app.set("views", path.join(__dirname, "pages", "index"));
-  app.set("view engine", "ejs");
+  app.set('views', `${__dirname}`);
+  app.use(express.static(`${__dirname}/public`));
+  app.set('view engine', 'ejs');
   // This is to read css
-  app.use(express.static(__dirname + "../public"));
   app.get("/", (_req, res) => {
     res.render(path.join(__dirname, "pages", "index"));
   });
