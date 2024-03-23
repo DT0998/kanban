71efe5822f7716856c94a3d641e4678e01b172e0
DT0998 <duongkytrieuvn@gmail.com>
2024-03-23 18:15:59 +0700

    fix: change connect

--- backend/api/utils/connect.js
+++ backend/api/utils/connect.js
@@ -1,11 +1,14 @@
 const logger = require("./logger");
 const mysql = require("mysql2");
 
-const connectionMysql = mysql.createConnection({
+const connectionMysql = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  connectionLimit: 10,
+  waitForConnections: true,
+  queueLimit: 0,
 });
 
 const connect = async () => {
