c9fc8c114dcb7b1364b28abaf774c6bac687e650
DT0998 <duongkytrieuvn@gmail.com>
2024-03-23 15:05:16 +0700

    fix: pino pretty

--- backend/API/utils/logger.js
+++ backend/API/utils/logger.js
@@ -1,17 +1,31 @@
 const logger = require("pino");
 const dayjs = require("dayjs");
+const logsPath = path.resolve("src", "logs");
 
-const log = logger({
-  base: {
-    pid: false,
-  },
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-    }
-  },
-  timestamp: () => `,"time":"${dayjs().format()}"`,
+const transport = logger.transport({
+  targets: [
+    {
+      target: "pino/file",
+      options: { destination: `${logsPath}/server.log` },
+      level: process.env.PINO_LOG_LEVEL || "info",
+    },
+    //@ts-ignore
+    {
+      target: "pino-pretty",
+      options: {
+        colorize: true,
+      },
+    },
+  ],
 });
+
+const log = logger(
+  {
+    level: process.env.PINO_LOG_LEVEL || "info",
+
+    timestamp: () => `,"time":"${dayjs().format()}"`,
+  },
+  transport
+);
 
 module.exports = log;
