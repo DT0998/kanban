33b047f8255af1b507be065ad0e1c6d3b6f937fb
Duong Trieu <duongkytrieuvn@gmail.com>
2024-03-24 14:03:53 +0700

    Delete frontend/src/environments/environments.ts

--- frontend/src/environments/environments.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  domainUrl: import.meta.env['NG_APP_DOMAIN_URL'],
-  contractAddress: import.meta.env['NG_APP_CONTRACT_ADDRESS'],
-  adminAddress: import.meta.env['NG_APP_ADMIN_ADDRESS'],
-  apiKeyAlchemy: import.meta.env['NG_APP_API_KEY_ALCHEMY'],
-};
