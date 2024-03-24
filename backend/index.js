be584d27ecced5d9b1cc7c665ae9f49ac9ec7c59
DT0998 <duongkytrieuvn@gmail.com>
2024-03-24 15:30:10 +0700

    fix: change cors

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 
 // config cors
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://kanban-api-mocha.vercel.app"],
+  origin: ["http://localhost:3030", "https://kanban-api-uez5.onrender.com"],
   optionsSuccessStatus: 200,
 };
 
