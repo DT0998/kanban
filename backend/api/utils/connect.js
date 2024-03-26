f04a63e8699963aa9004d436152ce8f31661506e
DT0998 <duongkytrieuvn@gmail.com>
2024-03-26 22:35:00 +0700

    feat: add port

--- backend/api/utils/connect.js
+++ backend/api/utils/connect.js
@@ -6,6 +6,7 @@ const connectionMysqlPool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 100,
   queueLimit: 0,
